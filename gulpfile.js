cfa070c5db627a4467b9dbd9849a83205cc84507
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2020-03-13 00:19:13 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const gulpInline = require('gulp-inline');
+const transform = require('gulp-transform');
+const openpgp = require('openpgp');
+const fs = require('fs');
+
+const password='lol';
+
+const decryptWrapper = fs.readFileSync('decrypt.html').toString();
+
+const encrypt = async function(content, file) {
+  const { data: encrypted } = await openpgp.encrypt({
+    message: openpgp.message.fromText(content),
+    passwords: [password],
+    compression: openpgp.enums.compression.zlib,
+  })
+  return decryptWrapper.replace('{{ content }}', encrypted);
+};
+
+exports.default = function () {
+  return gulp.src('page/index.html')
+  .pipe(gulpInline({
+    base: 'page',
+  }))
+  .pipe(transform('utf-8', encrypt))
+  .pipe(gulp.dest('dist/'))
+}
+
